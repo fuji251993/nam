db7c8aeacc96df6e9d70f72ff4e78e421e8fdc19
THKT53 Trần Xuân Nam <fujimaru251993@gmail.com>
2014-09-28 04:19:30 +0700

    4 commit

--- TangGiaoDien/Program.cs
+++ TangGiaoDien/Program.cs
@@ -14,7 +14,7 @@ namespace TangGiaoDien
         {
             Application.EnableVisualStyles();             
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Test());
+            Application.Run(new MH_DangNhap());
         }
     }
 }
